a7fd46e6c54bf55dc7e353a99a0a2ce13a15c99a
Erik <erik@anandb.(none)>
2013-10-31 16:49:00 -0500

    removed extra folder

--- ffmpeg4android/ffmpeg4android/gen/com/netcompss/ffmpeg4android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.netcompss.ffmpeg4android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- ffmpeg4android/ffmpeg4android/src/com/netcompss/ffmpeg4android/TrialExpiredRemoteException.java
+++
@@ -1,9 +0,0 @@
-package com.netcompss.ffmpeg4android;
-
-import android.os.RemoteException;
-
-public class TrialExpiredRemoteException extends RemoteException {
-
-	private static final long serialVersionUID = 1L;
-
-}
