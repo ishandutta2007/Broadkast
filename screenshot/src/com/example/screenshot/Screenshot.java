988bc23b296ab3ca03d2883ddcb594020b17adfb
Erik <erik@anandb.(none)>
2013-11-05 17:30:12 -0600

    actual screenshot folder

--- screenshot/src/com/example/screenshot/Screenshot.java
+++ screenshot/src/com/example/screenshot/Screenshot.java
@@ -7,14 +7,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.RemoteException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 import com.netcompss.ffmpeg4android_client.BaseWizard;
 import com.netcompss.ffmpeg4android_client.Prefs;
 
 public class Screenshot extends BaseWizard {
-	
+	String TAG = "Screenshot";
 	String fbpath = "/dev/graphics/fb0";
 	File tempBuff;
 	File images;
@@ -24,8 +26,23 @@ public class Screenshot extends BaseWizard {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        //copying code from demo client
+	    _prefs = new Prefs();
+	    _prefs.setContext(this);
+	      
         //get destination for output
-    	images = new File(Environment.getExternalStorageDirectory(), "screenshots");
+    	images = new File(getExternalFilesDir(null), "screenshots");
+    	
+    	//creating output file
+    	tempBuff = new File( getExternalFilesDir(null), "tempbuf");
+    	
+    	images.mkdirs();
+    	try {
+			tempBuff.createNewFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	
         
     }
@@ -39,27 +56,51 @@ public class Screenshot extends BaseWizard {
     }
     
     //Handles take screenshot event from pressing button
-    public void takeScreenshot( View view ) throws RemoteException, IOException, InterruptedException
+    public void takeScreenshot( View view )
     {
     	//First copy contents of /dev/graphics/fb0
     	//to temp file
+    	long startTime = System.currentTimeMillis();
+    	int numShots = 100;
     	
-    	Process p = Runtime.getRuntime().exec("su -c cat /dev/graphics/fb0 > " + images + "/tmpbuf");
-    	p.waitFor();
-    	
-    	//create output filename
-    	String outfilename = String.format("scan%d.png", System.currentTimeMillis());
-    	
-    	File outfile = new File( images.getPath(), outfilename);
-    	
-    	String out = outfile.getPath();
-    	//build ffmpeg command
-    	String command = "ffmpeg -vcodec rawvideo -f rawvideo -pix_fmt rgba -s 736x1280 -i /sdcard/screenshots/tmpbuf -f -image2 -vcodec png /sdcard/screenshots/out.png";
-    	
-    	//execute ffmpeg command
-    	remoteService.setFfmpegCommand(command);
-    	runWithCommand(command);
-    	
+	   // for( int i=0; i < numShots; i++){
+	    	
+	    	Process p = null;
+			try {
+				p = Runtime.getRuntime().exec("su -c cat " + fbpath + " > " + tempBuff.getAbsolutePath());
+				p.waitFor();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				Log.e( TAG, "cat failed");
+			} catch ( InterruptedException e2) {
+				e2.printStackTrace();
+				Log.e(TAG, "p.waitFor() interrupted");
+			}
+	    	
+	    	
+	    	if( p.exitValue() < 0 )
+	    	{
+	    		System.exit( -1 );
+	    	}
+	    	
+	    	//create output filename
+	    	String outfilename = String.format("scan%d.png", System.currentTimeMillis());
+	    	
+	    	File outfile = new File( images.getPath(), outfilename);
+	    	
+	    	//build ffmpeg command
+	    	String command = "ffmpeg -vframes 1 -vcodec rawvideo -f rawvideo -pix_fmt rgba -s 736x1300 -i " + tempBuff.getAbsolutePath()+ " -f image2 -vcodec png "+ outfile.getAbsolutePath();
+	    	
+	    	setCommand(command);
+			runTranscoing();
+	    	
+	    	//startAct(com.netcompss.ffmpeg4android_client.ShowFileAct.class);	
+	  //  }
+	    	
+		long endtime = System.currentTimeMillis();
+		
+		Log.i(TAG, "Total running time: " + (endtime - startTime) + "\nTime per screenshot: " + (endtime-startTime)/numShots);
     	
     	//have to check directory to see if it worked
     	
